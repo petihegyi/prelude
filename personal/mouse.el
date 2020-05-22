89bfec88d292a20252aa8ec33836fe1ba2205085
Hegyi Péter <frameskip0@gmail.com>
2020-05-22 18:05:10 +0200

    Added costumization for mouse forward and backward buttons

---
+++ personal/mouse.el
@@ -0,0 +1,3 @@
+;; Sets the forward and backward buttons on my mouse to show/hide the dired-sidebar
+(global-set-key (kbd "<mouse-8>") 'dired-sidebar-hide-sidebar)
+(global-set-key (kbd "<mouse-9>") 'dired-sidebar-show-sidebar)
